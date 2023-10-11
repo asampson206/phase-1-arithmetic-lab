812de28a3c0583c5db62f8b96b6b217b269752c4
alexander sampson <a.sampson206@gmail.com>
2023-10-10 19:52:10 -0700

    yeah

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
+let result = multiply
 
-// Write your code here
+let random = Math.floor(Math.random() + 1)
+Math.random()
+
+let mod = 4
+let num3 = 12
+let num4 = 5
+
+let max = Math.max(20)
+Math.max(1, 2, 3, 4, 20)
